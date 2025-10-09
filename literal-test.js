d30e4451a27dfa38a3b98e6530d1114a0e561310
Hao Liu <harryliuhao@hotmail.com>
2025-10-09 14:24:52 -0400

    new test code

---
+++ literal-test.js
@@ -0,0 +1,8 @@
+var greet = "Hello";
+var place = "World";
+console.log(greet + " " + place + "!"); //display both variables without using template literals
+
+
+greet = "Heya";
+//let place = "World";
+console.log(`${greet} ${place} !`) //display both variables using template literals
